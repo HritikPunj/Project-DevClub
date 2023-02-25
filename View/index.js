fac307f4fd205359afabb98b8ec3cf4c96c07e21
thuanquang98 <73714205+thuanquang98@users.noreply.github.com>
2023-02-25 14:10:45 -0500

    add more style rules, and update index.js

---
+++ View/index.js
@@ -0,0 +1,7 @@
+
+//functions runs when "As a driver" button clicked
+function displayDriverForm () {
+    const driver = document.querySelector(".driver-form");
+    driver.style.display = "block";
+}
+
